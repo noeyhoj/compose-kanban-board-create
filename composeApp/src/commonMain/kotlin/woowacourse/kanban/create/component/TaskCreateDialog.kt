50810a0a2324eccab910978c322dce39953b900e
ujeong <qnf323@naver.com>
2026-03-11 11:21:36 +0900

    feat: 태스크 생성 다이얼로그 추가

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/create/component/TaskCreateDialog.kt
@@ -0,0 +1,79 @@
+package woowacourse.kanban.create.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+@Preview(
+    showBackground = true,
+    widthDp = 672,
+    heightDp = 900,
+)
+fun TaskCreateDialog(modifier: Modifier = Modifier) {
+    Column(modifier = Modifier.background(color = Color.White)) {
+        DialogBar(
+            modifier = Modifier.padding(
+                vertical = 28.dp,
+                horizontal = 24.dp,
+            )
+                .fillMaxWidth(),
+        )
+        HorizontalDivider()
+        Column(
+            modifier = Modifier.padding(24.dp),
+            verticalArrangement = Arrangement.spacedBy(24.dp),
+        ) {
+            CommonTextColumn(
+                title = "제목 *",
+                placeHolder = "태스크 제목을 입력하세요",
+                height = 48.dp,
+            )
+            CommonTextColumn(
+                modifier = Modifier,
+                title = "설명",
+                placeHolder = "태스크에 대한 자세한 설명을 입력하세요",
+                height = 116.dp,
+                placeHolderAlignment = Alignment.TopStart,
+            )
+            CommonTextColumn(
+                modifier = Modifier,
+                title = "태그",
+                placeHolder = "태그를 쉼표로 구분하여 입력하세요 (예: 버그, 긴급)",
+                height = 44.dp,
+                hintText = "5자 이내의 태그를 최대 5개까지 등록할 수 있습니다.",
+            )
+            CommonButtonColumn(
+                modifier = Modifier.fillMaxWidth(),
+                header = "상태 *",
+                items = listOf(
+                    "To Do",
+                    "In Progress",
+                    "Done",
+                ),
+            ) { item -> StatusButton(status = item) }
+
+            CommonButtonColumn(
+                modifier = Modifier.fillMaxWidth(),
+                header = "담당자 *",
+                items = listOf(
+                    "다이노",
+                    "페임스",
+                ),
+            ) { item ->
+                CoachButton(name = item)
+            }
+            HorizontalDivider()
+            FooterRow()
+        }
+    }
+}
