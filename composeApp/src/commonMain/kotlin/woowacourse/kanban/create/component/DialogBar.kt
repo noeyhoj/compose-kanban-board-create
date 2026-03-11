f8ecd0b4efa0a3664d630dd30dbed3e6baa4faae
ujeong <qnf323@naver.com>
2026-03-11 12:43:01 +0900

    feat: 타이틀 로우 구현

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/create/component/DialogBar.kt
@@ -0,0 +1,31 @@
+package woowacourse.kanban.create.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter.Companion.tint
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview(showBackground = true)
+@Composable
+fun DialogBar(modifier: Modifier = Modifier) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier,
+    ) {
+        Text("새 태크스 생성", fontWeight = FontWeight.W600, fontSize = 20.sp)
+        Icon(imageVector = Icons.Default.Close, contentDescription = "닫기 버튼", modifier = Modifier.size(10.dp), tint = Color(0xFF6A7282))
+    }
+}
