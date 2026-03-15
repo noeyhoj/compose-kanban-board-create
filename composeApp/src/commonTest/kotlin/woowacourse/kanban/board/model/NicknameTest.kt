eadf0033408e74d9b027668ec1bb18047879ae3d
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2026-03-15 18:28:34 +0900

    test: 각 모델에 대해 성공하는 테스트를 추가로 작성함

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/model/NicknameTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/model/NicknameTest.kt
@@ -1,7 +1,8 @@
 package woowacourse.kanban.board.model
 
 import androidx.compose.ui.test.ExperimentalTestApi
-import org.junit.Assert
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Assert.assertThrows
 import org.junit.Test
 
 @OptIn(ExperimentalTestApi::class)
@@ -13,7 +14,7 @@ class NicknameTest {
         val nickname = ""
         // when
         // then
-        Assert.assertThrows(IllegalArgumentException::class.java) {
+        assertThrows(IllegalArgumentException::class.java) {
             Nickname(nickname)
         }
     }
@@ -24,8 +25,18 @@ class NicknameTest {
         val nickname = " "
         // when
         // then
-        Assert.assertThrows(IllegalArgumentException::class.java) {
+        assertThrows(IllegalArgumentException::class.java) {
             Nickname(nickname)
         }
+    }
+
+    @Test
+    fun `닉네임이 존재한다면 오류가 발생하지 않는다`() {
+        // given
+        val nickname = "다이노"
+        // when
+        val nicknameInfo = Nickname(nickname)
+        // then
+        assertThat(nicknameInfo.nickname).isEqualTo(nickname)
     }
 }
--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/model/TitleTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/model/TitleTest.kt
@@ -1,6 +1,7 @@
 package woowacourse.kanban.board.model
 
 import androidx.compose.ui.test.ExperimentalTestApi
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Assert
 import org.junit.Test
 
@@ -27,5 +28,15 @@ class TitleTest {
         Assert.assertThrows(IllegalArgumentException::class.java) {
             Title(title)
         }
+    }
+
+    @Test
+    fun `제목이 존재하는 경우 오류가 발생하지 않는다`() {
+        // given
+        val title = "이것은 제목입니다"
+        // when
+        val titleInfo = Title(title)
+        // then
+        assertThat(titleInfo.content).isEqualTo(title)
     }
 }
