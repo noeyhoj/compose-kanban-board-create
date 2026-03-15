5d509e5edbf7d82a2386d167ba6b405bf5ef9c08
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2026-03-15 13:23:55 +0900

    test: 최대 태그 수를 검증하는 테스트를 수정 및 오류가 발생하지 않는 테스트 추가

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/model/TagsTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/model/TagsTest.kt
@@ -1,20 +1,36 @@
 package woowacourse.kanban.board.model
 
 import androidx.compose.ui.test.ExperimentalTestApi
+import org.assertj.core.api.Assertions.assertThat
 import kotlin.test.Test
 import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThrows
+import woowacourse.kanban.board.constant.ERROR_TAG
 
 @OptIn(ExperimentalTestApi::class)
 class TagsTest {
 
     @Test
-    fun `태그의 개수가 5개 초과면 오류가 발생한다`() {
+    fun `태그의 개수가 최대 태그 수를 초과면 오류가 발생한다`() {
         // given
         val tags = listOf("일", "이", "삼", "사", "오", "육")
+        val maxTagSize = 5
         // when
         // then
-        Assert.assertThrows(IllegalArgumentException::class.java) {
-            Tags(tags)
+        assertThrows(IllegalArgumentException::class.java) {
+            Tags(tags = tags, maxTagSize = maxTagSize)
         }
+    }
+
+    @Test
+    fun `태그의 개수가 최대 태그 수 이하면 오류가 발생하지 않는다`() {
+        // given
+        val tags = listOf("일", "이", "삼", "사", "오")
+        val maxTagSize = 5
+        // when
+        val tagsData = Tags(tags = tags, maxTagSize = maxTagSize)
+        // then
+        assertThat(tagsData.tags).isEqualTo(tags)
     }
 }
