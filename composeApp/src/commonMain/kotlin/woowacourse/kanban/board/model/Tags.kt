5828e99b0a95e75b2376ed2b42ff539abed01389
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2026-03-17 14:20:40 +0900

    refactor: require을 수정함

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/Tags.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/Tags.kt
@@ -4,6 +4,6 @@ import woowacourse.kanban.board.constant.ERROR_TAG
 
 class Tags(val tags: List<String> = emptyList(), val maxTagSize: Int = 5) {
     init {
-        require(tags.size <= maxTagSize) { throw IllegalArgumentException("$ERROR_TAG 테그의 개수는 ${maxTagSize}개 이하로 작성해야 합니다.") }
+        require(tags.size <= maxTagSize) { "$ERROR_TAG 테그의 개수는 ${maxTagSize}개 이하로 작성해야 합니다." }
     }
 }
